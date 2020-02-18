b837cf3fa257b64eec3766674b4c66e7264d278c
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-02-18 22:59:51 +0700

    Commit

--- main.py
+++ main.py
@@ -27,5 +27,11 @@ async def on_member_remove(member):
     print("{0.user} has left the server.".format(member))
 
 
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+
 # run the bot
 client.run(token)
