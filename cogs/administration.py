75cc499d8ccce6099bd146b7d26055afc64f29d3
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-02-18 23:01:37 +0700

    Commit

--- cogs/administration.py
+++ cogs/administration.py
@@ -21,7 +21,7 @@ class Administration(commands.Cog):
         print('{0.name} was kicked from {0.guild}'.format(member))
 
     @commands.command()
-    async def ban(ctx, member: discord.Member, *, reason=None):
+    async def ban(self,ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
         print('{0.name} was banned from {0.guild}'.format(member))
 
