57ed2875773b097d819c1edeba5541ddd59c9dce
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-03-29 13:22:45 +0700

    commit

--- cogs/administration.py
+++ cogs/administration.py
@@ -21,9 +21,14 @@ class Administration(commands.Cog):
         print('{0.name} was kicked from {0.guild}'.format(member))
 
     @commands.command()
-    async def ban(self,ctx, member: discord.Member, *, reason=None):
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
         print('{0.name} was banned from {0.guild}'.format(member))
+
+    @commands.command()
+    async def nuke(self, ctx):
+        await ctx.channel.clone()
+        await ctx.channel.delete()
 
 
 def setup(client):
