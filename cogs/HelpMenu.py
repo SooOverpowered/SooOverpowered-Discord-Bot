e16ee4a62aff4e2769a882ef273bb7f69f8726f1
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-03-30 17:47:40 +0700

    commit

--- cogs/HelpMenu.py
+++ cogs/HelpMenu.py
@@ -1,20 +1,37 @@
 # Imports
 import discord
+from helper import *
 from discord.ext import commands
+
+
+# load up parameters
+try:
+    exec(open('parameters.py').read())
+except FileNotFoundError:
+    print('Unable to find parameters')
 
 
 class HelpMenu(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def help(self, ctx, arg=None):
-        embed = discord.Embed(
-            color=discord.Color.orange()
-        )
-        embed.set_author(name='Help')
-        embed.add_field(name='.ping', value='Returns Pong!', inline=False)
-        await ctx.send(embed=embed)
+    @commands.group()
+    async def help(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(
+                color=discord.Color.orange(),
+                author='Help',
+                description=f'Use `{prefix}help [module name]` for more information'
+            )
+            embed.add_field(name='**Administration**',
+                            value='Managing the server', inline=False)
+            embed.add_field(name='**Music**',
+                            value='The music player', inline=False)
+            await ctx.send(embed=embed)
+
+    @help.command()
+    async def Administration(self, ctx):
+        await ctx.send(embed=create_embed('test'))
 
 
 def setup(client):
--- main.py
+++ main.py
@@ -29,10 +29,9 @@ for filename in os.listdir('./cogs'):
 # System commands
 @client.command()
 async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
-    print(f"Cog **{extension}** reloaded successfully")
-    await ctx.send(embed=create_embed(f"Cog {extension} reloaded successfully"))
+    client.reload_extension(f'cogs.{extension}')
+    print(f"Cog {extension} reloaded successfully")
+    await ctx.send(embed=create_embed(f"Cog **{extension}** reloaded successfully"))
 
 
 @reload.error
@@ -44,15 +43,15 @@ async def reload_error(ctx, error):
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
-    print(f'Cog **{extension}** loaded successfully')
-    await ctx.send(embed=create_embed(f"Cog {extension} loaded successfully"))
+    print(f'Cog {extension} loaded successfully')
+    await ctx.send(embed=create_embed(f"Cog **{extension}** loaded successfully"))
 
 
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
-    print(f'Cog **{extension}** unloaded successfully')
-    await ctx.send(embed=create_embed(f"Cog {extension} unloaded successfully"))
+    print(f'Cog {extension} unloaded successfully')
+    await ctx.send(embed=create_embed(f"Cog **{extension}** unloaded successfully"))
 
 
 # Run the bot
