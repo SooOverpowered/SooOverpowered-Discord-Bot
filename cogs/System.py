d02c5011ad84ef1209c621a12101f5d74a38354a
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-03-31 10:28:23 +0700

    commit

--- cogs/System.py
+++ cogs/System.py
@@ -17,7 +17,9 @@ class System(commands.Cog):
 
     @reload.error
     async def reload_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(embed=create_embed(f'Cog not found, please use `{prefix}help reload` for list of cogs'))
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(embed=create_embed(f"Missing required argument, please use `{prefix}help reload` for correct usage"))
 
     @commands.command()
@@ -26,11 +28,17 @@ class System(commands.Cog):
         print(f'Cog {extension} loaded successfully')
         await ctx.send(embed=create_embed(f"Cog **{extension}** loaded successfully"))
 
+    @load.error
+    async def load_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(embed=create_embed(f'Cog not found, please use `{prefix}help reload` for list of cogs'))
+
     @commands.command()
     async def unload(self, ctx, extension):
         self.client.unload_extension(f'cogs.{extension}')
         print(f'Cog {extension} unloaded successfully')
         await ctx.send(embed=create_embed(f"Cog **{extension}** unloaded successfully"))
+
 
 def setup(client):
     client.add_cog(System(client))
