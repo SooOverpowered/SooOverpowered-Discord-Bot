ce9f0fcb09da73a35fb0a42d67d9110749793c74
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-04-01 22:04:40 +0700

    commit

--- cogs/music.py
+++ cogs/music.py
@@ -2,6 +2,7 @@
 import discord
 import youtube_dl
 import os
+import functools
 from helper import *
 from parameters import *
 from discord.ext import commands
@@ -48,6 +49,51 @@ class Music(commands.Cog):
                     'Bot was not connected to any voice channel'
                 )
             )
+
+    @commands.command(
+        name='play',
+        aliases=['p', ],
+        description='Play music from Youtube'
+    )
+    async def play(self, ctx, *, url):
+        channel = ctx.author.voice.channel
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+        video_source = get_video_info(url)
+        await ctx.send(
+            embed=create_embed(
+                f'**Now playing**: [{video_source[1]}]({url})'
+            )
+        )
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+        source = discord.PCMVolumeTransformer(
+            discord.FFmpegPCMAudio(
+                video_source[0],
+                before_options=" -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 1"),
+            volume=0.5
+        )
+        voice.play(source)
+
+        '''with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            print('Downloading audio now')
+            ydl.download([url])
+        for file in os.listdir('./'):
+            if file.endswith('.mp3'):
+                name = file
+                print(f'Renames file {file}')
+                os.rename(file, 'song.mp3')
+
+        voice.play(
+            discord.FFmpegPCMAudio('song.mp3'),
+            after=lambda e: print(f'{name} has finished playing')
+        )
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 0.07
+
+        await ctx.send(f'Playing {name}')
+        print('Playing')'''
 
 
 # Add cog
--- helper.py
+++ helper.py
@@ -1,11 +1,53 @@
 # Imports
 import discord
+import youtube_dl
+import os
+
+a = {'postprocessors': [{
+    'key': 'FFmpegExtractAudio',
+    'preferredcodec': 'mp3',
+    'preferredquality': '192'
+}]}
 
 
 # Create embed
 def create_embed(text):
-    embed=discord.Embed(
+    embed = discord.Embed(
         description=text,
         color=discord.Color.orange()
     )
     return embed
+
+
+def get_video_info(url_or_search):
+    ytdl_options = {
+        "default_search": "ytsearch",
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'extract_flat': 'in_playlist',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }]
+    }
+
+    def get_info(url):
+        with youtube_dl.YoutubeDL(ytdl_options) as ydl:
+            info = ydl.extract_info(url_or_search, download=False)
+            video = None
+            if "_type" in info and info["_type"] == "playlist":
+                return get_info(
+                    info['entries'][0]['url']
+                )
+            else:
+                video = info
+            return video
+    video = get_info(url_or_search)
+    video_format = video['formats'][0]
+    stream_url = video_format['url']
+    video_url = video['webpage_url']
+    title = video['title']
+    uploader = video["uploader"] if "uploader" in video else ""
+    thumbnail = video["thumbnail"] if "thumbnail" in video else None
+    return [stream_url,title]
