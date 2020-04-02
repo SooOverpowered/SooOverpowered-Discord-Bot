65f8576a3f74139eb80a6a105ad9ead2df428a10
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-04-02 21:03:29 +0700

    commit

--- cogs/music.py
+++ cogs/music.py
@@ -2,21 +2,102 @@
 import discord
 import youtube_dl
 import os
-import functools
+import asyncio
 from helper import *
 from parameters import *
 from discord.ext import commands
 
 
+def create_ytdl_source(source):
+    player = discord.PCMVolumeTransformer(
+        discord.FFmpegPCMAudio(
+            source,
+            before_options=" -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 1",
+        ),
+        volume=0.5
+    )
+    return player
+
+
+def get_video_info(url):
+    opts = {
+        "default_search": "ytsearch",
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'extract_flat': 'in_playlist',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }]
+    }
+
+    def get_info(url):
+        with youtube_dl.YoutubeDL(opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            video = None
+            if "_type" in info and info["_type"] == "playlist":
+                return get_info(
+                    info['entries'][0]['url']
+                )
+            else:
+                video = info
+            return video
+    video = get_info(url)
+    video_format = video['formats'][0]
+    stream_url = video_format['url']
+    video_url = video['webpage_url']
+    title = video['title']
+    uploader = video["uploader"] if "uploader" in video else ""
+    thumbnail = video["thumbnail"] if "thumbnail" in video else None
+    return [stream_url, title, video_url]
+
+
 class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.queues = {}
+        self.loop = None
+        self.now_playing = {}
+
+    def play_song(self, text_channel, voice):
+        info = get_video_info(self.now_playing[voice])
+        source = create_ytdl_source(info[0])
+        asyncio.run_coroutine_threadsafe(
+            text_channel.send(
+                embed=create_embed(
+                    f'**Now playing**: [{info[1]}]({info[2]})'
+                )
+            ), self.client.loop
+        )
+
+        def after_playing(err):
+            if self.queues[voice] != []:
+                info = self.queues[voice].pop(0)
+                if self.loop == 'all':
+                    self.queues[voice].append(info)
+                elif self.loop == 'one':
+                    self.queues[voice].insert(0, info)
+                self.now_playing[voice] = info
+                self.play_song(text_channel, voice, self.now_playing[voice])
+            else:
+                del self.now_playing[voice]
+                asyncio.run_coroutine_threadsafe(
+                    voice.disconnect(), self.client.loop)
+
+        voice.play(source, after=after_playing)
 
     @commands.command(
         name='join',
         description='Connect to your current voice channel'
     )
     async def join(self, ctx):
+        if ctx.author.voice == None:
+            await ctx.send(
+                embed=create_embed(
+                    "You are not in any voice channel"
+                )
+            )
         channel = ctx.author.voice.channel
         voice = ctx.voice_client
         if voice != None and voice.channel == channel:
@@ -27,10 +108,13 @@ class Music(commands.Cog):
             )
         elif voice != None and voice.is_connected:
             await voice.move_to(channel)
-            print(f'Bot moved to {channel}')
         else:
             voice = await channel.connect()
-            print(f'Bot connected to {channel}')
+            await ctx.send(
+                embed=create_embed(
+                    f'Bot connected to **{channel}**'
+                )
+            )
 
     @commands.command(
         name='leave',
@@ -41,11 +125,12 @@ class Music(commands.Cog):
         voice = ctx.voice_client
         if voice != None:
             channel = voice.channel
+            del self.queues[voice]
+            del self.now_playing[voice]
             await voice.disconnect()
-            print(f'The bot has left {channel}')
             await ctx.send(
                 embed=create_embed(
-                    f'Bot disconnected from {channel}'
+                    f'Bot disconnected from **{channel}**'
                 )
             )
         else:
@@ -61,34 +146,28 @@ class Music(commands.Cog):
         description='Play music from Youtube'
     )
     async def play(self, ctx, *, url):
+        text_channel = ctx.channel
         channel = ctx.author.voice.channel
         voice = ctx.voice_client
         if voice != None and voice.is_playing():
+            video_source = get_video_info(url)
+            self.queues[voice].append(url)
+            await ctx.channel.purge(limit=1)
             await ctx.send(
                 embed=create_embed(
-                    'Already playing music'
+                    f'Song [{video_source[1]}]({video_source[2]}) added to queue'
                 )
             )
         else:
-            video_source = get_video_info(url)
             if voice != None and voice.is_connected():
                 await voice.move_to(channel)
             else:
                 voice = await channel.connect()
-            source = discord.PCMVolumeTransformer(
-                discord.FFmpegPCMAudio(
-                    video_source[0],
-                    before_options=" -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 1"
-                ),
-                volume=0.5
-            )
-            voice.play(source)
+            voice = ctx.voice_client
+            self.queues[voice] = []
+            self.now_playing[voice] = url
+            self.play_song(text_channel, voice)
             await ctx.channel.purge(limit=1)
-            await ctx.send(
-                embed=create_embed(
-                    f'**Now playing**: [{video_source[1]}]({video_source[2]})'
-                )
-            )
 
     @commands.command(
         name='pause',
@@ -152,6 +231,8 @@ class Music(commands.Cog):
     async def stop(self, ctx):
         voice = ctx.voice_client
         if voice != None and voice.is_playing():
+            del self.queues[voice]
+            del self.now_playing[voice]
             voice.stop()
             await ctx.send(
                 embed=create_embed(
@@ -164,6 +245,50 @@ class Music(commands.Cog):
                     'Cannot stop if no music is playing'
                 )
             )
+
+    @commands.command(
+        name='queue',
+        aliases=['q', ],
+        description='Display the current music queue'
+    )
+    async def queue(self, ctx, arg=None):
+        if arg != None:
+            await ctx.send(
+                embed=create_embed(
+                    'The queue command does not take in any other argument'
+                )
+            )
+        elif self.queue[ctx.voice_client] == []:
+            ctx.send(
+                embed=create_embed(
+                    'The music queue is empty'
+                )
+            )
+        else:
+            pass
+
+    @commands.command()
+    async def loop(self, ctx, arg=None):
+        if arg == None:
+            self.loop = 'all'
+            voice = ctx.voice_client
+            self.queues[voice].append(self.now_playing[voice])
+            await ctx.send(
+                embed=create_embed(
+                    'Repeating all songs in the music queue'
+                )
+            )
+        elif arg == 'one':
+            self.loop = 'one'
+            voice = ctx.voice_client
+            self.queues[voice].append(self.now_playing[voice])
+            await ctx.send(
+                embed=create_embed(
+                    'Repeating the current song in the music queue'
+                )
+            )
+        elif arg == 'off':
+            self.loop = None
 
 
 # Add cog
--- helper.py
+++ helper.py
@@ -3,12 +3,6 @@ import discord
 import youtube_dl
 import os
 
-a = {'postprocessors': [{
-    'key': 'FFmpegExtractAudio',
-    'preferredcodec': 'mp3',
-    'preferredquality': '192'
-}]}
-
 
 # Create embed
 def create_embed(text):
@@ -17,37 +11,3 @@ def create_embed(text):
         color=discord.Color.orange()
     )
     return embed
-
-
-def get_video_info(url):
-    opts = {
-        "default_search": "ytsearch",
-        'format': 'bestaudio/best',
-        'quiet': True,
-        'extract_flat': 'in_playlist',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192'
-        }]
-    }
-
-    def get_info(url):
-        with youtube_dl.YoutubeDL(opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            video = None
-            if "_type" in info and info["_type"] == "playlist":
-                return get_info(
-                    info['entries'][0]['url']
-                )
-            else:
-                video = info
-            return video
-    video = get_info(url)
-    video_format = video['formats'][0]
-    stream_url = video_format['url']
-    video_url = video['webpage_url']
-    title = video['title']
-    uploader = video["uploader"] if "uploader" in video else ""
-    thumbnail = video["thumbnail"] if "thumbnail" in video else None
-    return [stream_url, title, video_url]
