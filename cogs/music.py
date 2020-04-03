44ceac1f98909268ef108188974c0e2011adab94
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-04-03 14:25:55 +0700

    commit

--- cogs/music.py
+++ cogs/music.py
@@ -9,6 +9,11 @@ from parameters import *
 from discord.ext import commands
 
 
+discord.opus.load_opus()
+if not discord.opus.is_loaded():
+    raise RunTimeError('Opus failed to load')
+
+
 def create_ytdl_source(source):
     player = discord.PCMVolumeTransformer(
         discord.FFmpegPCMAudio(
