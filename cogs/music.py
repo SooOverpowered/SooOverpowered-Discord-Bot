14e14376a7348215acf31b2ab5a14d9cdff545a5
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-04-03 14:04:31 +0700

    commit

--- cogs/music.py
+++ cogs/music.py
@@ -13,7 +13,7 @@ def create_ytdl_source(source):
     player = discord.PCMVolumeTransformer(
         discord.FFmpegPCMAudio(
             source,
-            executable='./ffmpeg/bin',
+            executable='./ffmpeg/bin/ffmpeg.exe',
             before_options=" -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 1",
         ),
         volume=0.5
