b2cecf948c7430b79d9b0ddff4b3a74f602bfda2
SooOverpowered <49089778+SooOverpowered@users.noreply.github.com>
2020-04-07 14:22:54 +0700

    commit

--- cogs/administration.py
+++ cogs/administration.py
@@ -296,6 +296,11 @@ class Administration(commands.Cog, name='Administration'):
 
     # Events
     @commands.Cog.listener()
+    async def on_disconnect(self):
+        await self.client.connect(reconnect=True)
+        await self.client.login(token, bot=True)
+
+    @commands.Cog.listener()
     async def on_connect(self):
         await self.client.change_presence(
             activity=discord.Activity(
