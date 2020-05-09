6ac5e6221434b41fab6c73624b0bc2b950a1cfbd
SooOverpowered <lehoangminh15122000@gmail.com>
2020-05-09 14:15:43 +0700

    commit

--- cogs/System.py
+++ cogs/System.py
@@ -253,8 +253,6 @@ class System(commands.Cog, name='System'):
         queuecol.delete_many({'guild_id': guild.id})
         playlistcol.delete_many({'guild_id': guild.id})
 
-
-'''
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
@@ -265,7 +263,6 @@ class System(commands.Cog, name='System'):
                 delete_after=10
             )
             await ctx.message.delete()
-'''
 
 
 def setup(client):
